91c258da7b7214d30f42323942252543ac5076a6
Sharmila127 <sharmila6201@gmail.com>
2025-12-11 13:31:22 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,11 +15,11 @@ function Login() {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Email: </label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
         </div>
         <div>
           <label>Password: </label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
         </div>
         <button type="submit">Login</button>
       </form>
