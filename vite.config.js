aac2c4501ee9eb3e71279ea6ffa504a7ea675c88
Sharmila127 <sharmila6201@gmail.com>
2025-12-11 13:29:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',       // '/' because app is at root of domain
+  base: '/',
   build: {
     outDir: 'build',
-  },
-  server: {
-    port: 3000,
   },
 });
